44b5f467afab72650eca38ae7aaa997003247f9c
kartik <kartikchopade90@gmail.com>
2022-07-31 23:55:00 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { CourceComponent } from './cource/cource.component';
+import { HomeComponent } from './home/home.component';
+import { JoinnowComponent } from './joinnow/joinnow.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'cource',component:CourceComponent},
+  {path:'joinnow',component:JoinnowComponent},
+  {path:'about',component:AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
