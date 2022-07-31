f1b62c715be7262cb88465ff8fa945a99852d7be
kartik <kartikchopade90@gmail.com>
2022-08-01 00:25:00 +0530

    first commit

--- src/app/cource/cource.component.ts
+++ src/app/cource/cource.component.ts
@@ -12,10 +12,10 @@ export class CourceComponent implements OnInit {
   ngOnInit(): void {
   }
 course=[
-  {'id':1,'name':'Learn Angular','description':'Angular is an application design framework and development platform for creating efficient and sophisticated single-page apps.','image':'../../assets/angular.jpg'},
-  {'id':2,'name':'Learn Java','description':'Java is a high-level, class-based, object-oriented programming language. Java is a popular programming language. ','image':'../../assets/java.jpg'},
-  {'id':3,'name':'Learn .Net','description':'.NET is a free, cross-platform, open source developer platform for building many different types of applications.','image':'../../assets/net.jpg'},
-  {'id':4,'name':'Learn Python','description':'Python is a high-level, interpreted, general-purpose programming language. Easy to use.','image':'../../assets/python.jpg'},
+  {'id':1,'name':'Learn Angular','description':'Angular is an application design framework and development platform for creating efficient and sophisticated single-page apps.','image':'assets/angular.jpg'},
+  {'id':2,'name':'Learn Java','description':'Java is a high-level, class-based, object-oriented programming language. Java is a popular programming language. ','image':'assets/java.jpg'},
+  {'id':3,'name':'Learn .Net','description':'.NET is a free, cross-platform, open source developer platform for building many different types of applications.','image':'assets/net.jpg'},
+  {'id':4,'name':'Learn Python','description':'Python is a high-level, interpreted, general-purpose programming language. Easy to use.','image':'assets/python.jpg'},
 
 ]
 }
